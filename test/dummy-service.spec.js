cda24ab47ad49135a33e21deebbc049a60fb4bce
atorma <anssi.torma@iki.fi>
2015-03-05 20:40:00 +0200

    Got the first angular test to run using node.js

--- test/dummy-service.spec.js
+++ test/dummy-service.spec.js
@@ -2,9 +2,26 @@
 
 var angular = require("angular");
 require("angular-mocks/ngMock");
+require("../src/debt");
 
 describe("dummyService", function() {
 	
-	beforeEach(module("debtApp"));
+	var $httpBackend;
+	var dummyService;
+	
+	beforeEach(angular.mock.module("debtApp"));
+	
+	beforeEach(angular.mock.inject(function(_$httpBackend_, _dummyService_) {
+		$httpBackend = _$httpBackend_;
+		dummyService = _dummyService_;
+	}));
+	
+	it("calls correct url", function() {
+		$httpBackend.expectGET("http://www.hs.fi").respond(200);
+		
+		dummyService.getHesari();
+		
+		$httpBackend.flush();
+	});
 	
 });
