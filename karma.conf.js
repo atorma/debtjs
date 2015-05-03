93b0e944c0fbd80d15f6b6717c2a4f78a7e207e2
atorma <anssi.torma@iki.fi>
2015-05-03 08:24:57 +0300

    Using Chrome to run Jasmine tests instead of slow PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
 		colors : true,
 		logLevel : config.LOG_INFO, // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		autoWatch : true,
-		browsers : [ 'PhantomJS' ],
+		browsers : [ 'Chrome' ],
 		singleRun : false
 	});
 };
