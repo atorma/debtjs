90002affe21ce46127ac3f8c2a274727c69fd5ef
atorma <anssi.torma@iki.fi>
2015-05-09 14:35:17 +0300

    Karma loads source map files

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
 	config.set({
-		basePath : 'build',
-		files : ['libs.js', 'debt.spec.js'],
+		basePath : './build',
+		files : ['libs.js', 'debt.js', 'debt.spec.js', {pattern: '**/*.js.map', included: false}],
 		exclude : [],
 		frameworks : ['jasmine'],
 		reporters : ['mocha'],
