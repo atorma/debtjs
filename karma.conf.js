ea13cf3b5a33dd9eddc55698d1684cbbc00fe40c
atorma <anssi.torma@iki.fi>
2016-03-07 08:14:43 +0200

    Karma log level to INFO.

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
     },
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG, // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO, // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     autoWatch: true,
     singleRun: false
   });
