a105d8cc7d8977577eebfbebbd1059969fb5aa33
atorma <anssi.torma@iki.fi>
2016-03-10 07:06:35 +0200

    debtService throws an error if it cannot convert participation to given currency

--- src/app/debts/debt-service.spec.js
+++ src/app/debts/debt-service.spec.js
@@ -11,17 +11,17 @@ describe("debtService", function() {
   var debtService;
 
   var solveDebts;
-  var debts;
+  var mockDebts;
 
   beforeEach(function() {
-    debts = [
+    mockDebts = [
       {debtor: {id: 1, name: "Valtteri"}, creditor: {id: 9, name: "Anssi"}, amount: 10},
       {debtor: {id: 2, name: "Johannes"}, creditor: {id: 8, name: "Tomi"}, amount: 15},
       {debtor: {id: 2, name: "Johannes"}, creditor: {id: 9, name: "Anssi"}, amount: 27}
     ];
 
     solveDebts = jasmine.createSpy("solveDebts");
-    solveDebts.and.returnValue(debts);
+    solveDebts.and.returnValue(mockDebts);
   });
 
   beforeEach(angular.mock.module("debtApp", function($provide) {
@@ -35,30 +35,34 @@ describe("debtService", function() {
 
   describe("computeDebts", function() {
     var balanceSheet;
+    var p1, p2, e1;
+    var p1e1, p2e1;
+    var participations;
 
     beforeEach(function() {
       balanceSheet = new BalanceSheet();
+      p1 = balanceSheet.createPerson();
+      p2 = balanceSheet.createPerson();
+      e1 = balanceSheet.createExpense();
+      p1e1 = balanceSheet.createParticipation({person: p1, expense: e1, paid: 10, share: 5});
+      p2e1 = balanceSheet.createParticipation({person: p2, expense: e1, paid: 0, share: 5});
+      participations = [p1e1, p2e1];
     });
 
     it("uses injected debtSolver", function() {
-      var participations = [{person: {id: 1}, expense: {id: 2}, paid: 10, share: 10}];
-      solveDebts.and.returnValue(debts);
+      solveDebts.and.returnValue(mockDebts);
 
       var result = debtService.computeDebts(participations);
 
       expect(solveDebts).toHaveBeenCalledWith(participations);
-      expect(result).toBe(debts);
+      expect(result).toBe(mockDebts);
     });
 
     it("converts participations to given currency if defined and the currency to computed debts", function() {
-      var p1 = balanceSheet.createPerson();
-      var p2 = balanceSheet.createPerson();
-      var e1 = balanceSheet.createExpense({currency: "EUR"});
-      var p1e1 = balanceSheet.createParticipation({person: p1, expense: e1, paid: 10, share: 5});
-      var p2e1 = balanceSheet.createParticipation({person: p2, expense: e1, paid: 0, share: 5});
+      e1.currency = "EUR";
       balanceSheet.addOrUpdateExchangeRate({fixed: "EUR", variable: "DKK", rate: 7.2313});
 
-      var result = debtService.computeDebts([p1e1, p2e1], "DKK");
+      var result = debtService.computeDebts(participations, "DKK");
 
       expect(solveDebts).toHaveBeenCalledWith([
         jasmine.objectContaining({paid: 72.31, share: 36.16}),
@@ -71,14 +75,20 @@ describe("debtService", function() {
 
     });
 
+    it("throws error if a participation cannot be converted to requested currency", function() {
+      e1.currency = "EUR";
+
+      expect(function() {
+        debtService.computeDebts(participations, "DKK");
+      }).toThrow();
+    });
+
   });
-
-
 
 
   it("organizeByDebtor organizes debts by debtor", function() {
 
-    var debtsByDebtor = debtService.organizeByDebtor(debts);
+    var debtsByDebtor = debtService.organizeByDebtor(mockDebts);
 
     expect(debtsByDebtor.length).toBe(2);
     expect(debtsByDebtor[0]).toEqual({
