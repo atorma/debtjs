28bfc62170b5f32cdc5bd5019b3de37151c8bb95
atorma <anssi.torma@iki.fi>
2016-03-29 07:04:59 +0300

    Testing if usePolling: true helps IntelliJ to pick up changes when running Karma tests w/ Browserify.

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,8 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO, // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     autoWatch: true,
-    singleRun: true
+    singleRun: true,
+    usePolling: true
   });
 };
 
